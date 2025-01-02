0a3547837298b50a7e2c20b0773d017d85484274
Leticia Jyo <leticia.jyo@gmail.com>
2025-01-02 14:57:41 +0100

    refactor: separate components in the Header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,51 +1,22 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useScroll } from '../hooks/useScroll';
+import { Logo } from './Logo';
+import { NavLinks } from './NavLinks';
+import { UserProfile } from './UserProfile';
 
 export default function Header() {
-  const [isScrolled, setIsScrolled] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+  const isScrolled = useScroll();
 
   return (
     <header
-      className={` ${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6`}
+      className={`${isScrolled && 'bg-black'} fixed top-0 z-50 flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 transition-all lg:px-10 lg:py-4`}
     >
       <div className='flex items-center space-x-2 md:space-x-8'>
-        <img
-          src='https://rb.gy/ulxxee'
-          alt='netflix'
-          width={120}
-          height={120}
-        />
-        <ul className='hidden md:flex md:space-x-4'>
-          <li>Home</li>
-          <li>TV Shows</li>
-          <li>Movies</li>
-          <li>Latest</li>
-        </ul>
+        <Logo />
+        <NavLinks />
       </div>
-      <div className='flex items-center space-x-4'>
-        <p className='hidden cursor-not-allowed lg:inline'>Kids</p>
-        <img
-          src='https://rb.gy/g1pwyx'
-          alt=''
-          className='cursor-pointer rounded'
-        />
-      </div>
+      <UserProfile />
     </header>
   );
 }

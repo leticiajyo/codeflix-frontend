7ba37fba4add4d73c4d614857c9451096b732f0f
Leticia Jyo <leticia.jyo@gmail.com>
2025-01-10 17:48:27 +0100

    feat: add movie row component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
-import Image from 'next/image';
 import Header from './components/Header';
-import { InformationCircleIcon, PlayIcon } from '@heroicons/react/24/outline';
 import { Banner } from './components/Banner';
+import { MovieRow } from './components/MovieRow';
 
 export default function Home() {
   return (
-    <div className='relative h-screen overflow-hidden bg-gradient-to-b lg:h-[140vh]'>
+    <div className='relative bg-gradient-to-b pb-8'>
       <Header />
-      <main className='relative pb-24 pl-4 lg:pl-16'>
+      <main className='relative mb-48 h-screen pl-4 lg:pl-16'>
         <Banner />
+        <MovieRow sectionTitle='Popular' />
+        <MovieRow sectionTitle='Trending' />
+        <MovieRow sectionTitle='Top Rated' />
       </main>
     </div>
   );
